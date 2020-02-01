a5592b4bfe21d2954a16f25794f828b2da999a55
gogiii <gvm@nm.ru>
2020-02-01 18:10:40 +0300

    ortho fix

--- giggle.js
+++ giggle.js
@@ -27,10 +27,10 @@ export let utils = {
     },
     createOrtho(l, r, b, t, n, f) {
         return [    // as in gl specs
-            2/(r-l), 0, 0, -(r+l)/(r-l),
-            0, 2/(t-b), 0, -(t+b)/(t-b),
-            0, 0, -2/(f-n), -(f+n)/(f-n),
-            0, 0, 0, 1
+            2/(r-l), 0, 0, 0,
+            0, 2/(t-b), 0, 0,
+            0, 0, -2/(f-n), 0,
+            -(r+l)/(r-l), -(t+b)/(t-b), -(f+n)/(f-n), 1
         ];
     }
 };
